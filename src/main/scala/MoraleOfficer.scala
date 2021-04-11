dabb95eea18c18bdb4436dc796be309d28e3d16d
SimunKaracic <simunkaracic@gmail.com>
2021-04-11 18:26:23 +0200

    More better

--- src/main/scala/MoraleOfficer.scala
+++ src/main/scala/MoraleOfficer.scala
@@ -1,8 +1,10 @@
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
-import zio.blocking.effectBlocking
+import sttp.client3.{RequestT, UriContext, basicRequest}
+import zio.blocking.{Blocking, effectBlocking}
+import zio.clock.Clock
 import zio.console.putStrLn
-import zio.{ExitCode, Schedule, URIO, ZIO}
+import zio.{ExitCode, Schedule, URIO, ZIO, random}
 import zio.duration.durationInt
 
 import java.awt.Desktop
@@ -15,24 +17,25 @@ object MoraleOfficer extends zio.App {
   override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = (for {
     _ <- putStrLn("Checking out subs...")
     subs <- ZIO.foreachPar(catSubs) { sub =>
-      get_subreddit(sub)
+      get_subreddit(sub).zipLeft(putStrLn(s"Gathering posts for ${sub}"))
     }
     _ <- putStrLn("Gathering cattiest posts")
     posts = subs.flatMap(extract_top_posts).toSet
     _ <- putStrLn("Frantically opening browser tabs so you don't see the errors in the output")
-    _ <- ZIO.foreachPar(posts) { post =>
+    _ <- ZIO.foreachPar_(posts) { post =>
       open_in_browser(s"https://old.reddit.com${post}")
     }
     _ <- putStrLn("Morale successfully officered!")
   } yield ()).exitCode
 
-  def get_subreddit(sub: String) = {
+  private def get_subreddit(sub: String) = {
     get_html(s"https://old.reddit.com/r/${sub}")
   }
 
-  def extract_top_posts(doc: Document) = {
+  private def extract_top_posts(doc: Document) = {
     val thumbnails = doc
       .getElementsByAttributeValue("data-event-action", "thumbnail")
+
     val links = (for (tn <- thumbnails.asScala if !tn.html().contains("- announcement")) yield tn.attr("href"))
       .filter(x => x.contains("/r/"))
       .filter(x => x.contains("comments"))
@@ -41,7 +44,7 @@ object MoraleOfficer extends zio.App {
     links
   }
 
-  def open_in_browser(link: String) = {
+  private def open_in_browser(link: String) = {
     effectBlocking(
       if (Desktop.isDesktopSupported && Desktop.getDesktop.isSupported(Desktop.Action.BROWSE)) {
         Desktop.getDesktop.browse(new URI(link))
@@ -50,34 +53,30 @@ object MoraleOfficer extends zio.App {
       .orDie
   }
 
-  def get_html(url: String) = {
+  private def get_html(url: String): URIO[Any with random.Random with Blocking with Clock, Document] = {
     val doc = Jsoup.connect(url)
-      .header("user-agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0").get()
+      .header("user-agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:47.0) Gecko/20100101 Firefox/47.0")
 
-    effectBlocking(doc).refineToOrDie[IOException]
+    effectBlocking(doc.get())
+      .refineToOrDie[IOException]
       .delay(Random.nextInt(3).seconds)
       .retry((Schedule.exponential(100.millis) && Schedule.recurs(20)).jittered)
       .orDie
   }
-  val catSubs: List[String] = List("CatLoaf",
+
+  private val catSubs = List("CatLoaf",
     "Floof",
     "Blep",
     "CatsStandingUp",
     "CatBellies",
     "DelightfullyChubby",
-    "CatsLookingSeductive",
-    "CatConspiracy",
-    "CatLogic",
     "Meow_Irl",
     "Cats",
-    "CatSpotting",
     "CatGifs",
-    "KittenGifs",
-    "CatVideos",
     "CatsWhoYell",
     "TuckedInKitties",
-    "NorwegianForestCats",
     "BabyBigCatGifs",
     "BigCats",
+    "catdimension",
   )
 }
