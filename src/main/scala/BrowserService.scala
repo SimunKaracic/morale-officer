fa8a50c251814d6b8381041512c7695f4a617cf1
SimunKaracic <simunkaracic@gmail.com>
2021-05-14 21:57:08 +0200

    Amazing debug message

--- src/main/scala/BrowserService.scala
+++ src/main/scala/BrowserService.scala
@@ -13,6 +13,8 @@ object BrowserService {
     effectBlocking(
       if (Desktop.isDesktopSupported && Desktop.getDesktop.isSupported(Desktop.Action.BROWSE)) {
         Desktop.getDesktop.browse(new URI(url))
+      } else {
+        println("There's no desktop browser support?? Open a issue, I guess")
       }).refineToOrDie[IOException]
       .retry(Schedule.recurs(30) && Schedule.spaced(150.millis))
       .orElse(putStrLn(s"Failed to open $url in browser"))
